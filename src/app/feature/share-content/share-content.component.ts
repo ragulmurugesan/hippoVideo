0a1fbb3bf1c7f158951561750c8ca32d42fb6cb0
Blackwolf37 <ragul.murugesan@live.com>
2019-07-28 02:30:22 +0530

    Share content features update

--- src/app/feature/share-content/share-content.component.ts
+++ src/app/feature/share-content/share-content.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./share-content.component.scss']
 })
 export class ShareContentComponent implements OnInit {
+  shareFeaturesInfo: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.initialize();
+  }
+
+  initialize() {
+    this.shareFeaturesInfo = [
+      { id: 1, title: 'Share', logo: 'share-1', description: 'Share your video online via social channels or email' },
+      { id: 2, title: 'Export', logo: 'share-2', description: 'Export videos to Youtube, Vimeo and Google Suite' },
+      { id: 3, title: 'Embed', logo: 'share-3', description: 'Embed your videos anywhere. Adapts to any screen.' },
+      { id: 4, title: 'Guest Record', logo: 'share-4', description: 'Ask anyone to record a video from a shareable link' }
+    ]
   }
 
 }
