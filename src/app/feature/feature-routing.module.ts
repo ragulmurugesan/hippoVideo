834f6fadae00db91aa563248ff23ff990b3fdd56
Blackwolf37 <ragul.murugesan@live.com>
2019-07-27 16:52:20 +0530

    Feature header content update

--- src/app/feature/feature-routing.module.ts
+++ src/app/feature/feature-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FeaturesComponent } from './features/features.component';
+import { FeaturesComponent } from './features.component';
 
 const routes: Routes = [
     { path: '', component: FeaturesComponent }
--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeatureHeaderComponent } from './feature-header/feature-header.component';
 import { MainContentComponent } from './main-content/main-content.component';
-import { FeaturesComponent } from './features/features.component';
 import { FeatureRoutingModule } from './feature-routing.module';
+import { FeaturesComponent } from './features.component';
 
 @NgModule({
   declarations: [FeaturesComponent, FeatureHeaderComponent,
