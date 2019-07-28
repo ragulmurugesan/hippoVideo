f7e3ed65c137f162a955bbfe7a7b56a90072318c
Blackwolf37 <ragul.murugesan@live.com>
2019-07-28 10:53:11 +0530

    Share content update

--- src/app/feature/share-content/share-content.component.ts
+++ src/app/feature/share-content/share-content.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShareContentComponent implements OnInit {
   shareFeaturesInfo: any;
+  integrationInfo: string[];
 
   constructor() { }
 
@@ -20,7 +21,14 @@ export class ShareContentComponent implements OnInit {
       { id: 2, title: 'Export', logo: 'share-2', description: 'Export videos to Youtube, Vimeo and Google Suite' },
       { id: 3, title: 'Embed', logo: 'share-3', description: 'Embed your videos anywhere. Adapts to any screen.' },
       { id: 4, title: 'Guest Record', logo: 'share-4', description: 'Ask anyone to record a video from a shareable link' }
-    ]
+    ];
+    this.integrationInfo = [
+      'fe-d2l',
+      'fe-gdocs',
+      'fe-slack',
+      'fe-trello',
+      'fe-gmail'
+    ];
   }
 
 }
