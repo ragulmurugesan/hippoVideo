0e026a9018b4f14b2506740e6c3fb91a0c5d04a7
Blackwolf37 <ragul.murugesan@live.com>
2019-07-29 21:22:49 +0530

    Media query updated

--- src/app/utils/js/animation.js
+++ src/app/utils/js/animation.js
@@ -1,29 +1,23 @@
 
-// var parallaxEls;
-
-// window.addEventListener('DOMContentLoaded', () => {
-//     parallaxEls = document.querySelectorAll("[data-speed]");
-// })
-
 window.addEventListener('scroll', () => {
     const el1 = document.querySelector('.animate-1');
     const el2 = document.querySelector('.animate-2');
     const el3 = document.querySelector('.animate-3');
     const parallaxEls = document.querySelectorAll("[data-speed]");
     let banner = document.querySelector('.banner');
-    let smartMove = document.querySelector('.parallex-para');
-    let smMoveHeader = document.querySelector('.heading.h--margin-0.text-capitalize');
+    // let smartMove = document.querySelector('.parallex-para');
+    // let smMoveHeader = document.querySelector('.heading.h--margin-0.text-capitalize');
     if (banner && banner.getBoundingClientRect().bottom > 10) {
         handleBannerParallex(parallaxEls);
     }
     if (el1 || el2 || el3) {
         animate(el1, el2, el3);
     }
-    if (isInViewPort(smMoveHeader)) {
-        let obj = [smartMove];
-        handleBannerParallex(obj)
-    }
-    console.log('The smartMove values are', smartMove ? smartMove.getBoundingClientRect() : null);
+    // if (isInViewPort(smMoveHeader)) {
+    //     let obj = [smartMove];
+    //     handleBannerParallex(obj)
+    // }
+    // console.log('The smartMove values are', smartMove ? smartMove.getBoundingClientRect() : null);
 })
 
 
@@ -48,30 +42,14 @@ function handleBannerParallex(parallaxEls) {
         } else if (parallaxEl.classList.contains("banner-subtitle")) {
             parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0)`;
         }
-        else if (parallaxEl.classList.contains('parallex-para')) {
-            parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0)`;
-        }
+        // else if (parallaxEl.classList.contains('parallex-para')) {
+        //     parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0)`;
+        // }
         else {
             parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0)`;
         }
     }
 }
-
-// function smartMoveParallex(parallaxEl){
-//    //for (const parallaxEl of parallaxEls) {
-//         const direction = parallaxEl.dataset.direction == "up" || parallaxEl.dataset.direction == "left" ? "-" : "";
-//         const transformY = this.pageYOffset * parallaxEl.dataset.speed;
-//         if (parallaxEl.classList.contains("banner-title")) {
-//             parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0)`;
-//         } else if (parallaxEl.classList.contains("banner-subtitle")) {
-//             parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0)`;
-//         }
-//         else {
-//             parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0)`;
-//         }
-//    // }
-// }
-
 
 function isInViewPort(elem) {
     if (elem) {
